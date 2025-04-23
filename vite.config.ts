e3ee047d249cbb2118bb1abb5d900e71c023ed17
buzair6 <buzair6@gmail.com>
2025-04-23 08:11:21 +0500

    improved

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-// vite.config.js
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // ... other config
-  base: "/vitejs-vite-zqovbswf",
+  // ... keep any other configurations you might have
+  base: "/vitejs-vite-zqovbswf/", // <--- Ensure the trailing slash is here
 });
